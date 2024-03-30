d670448002590bdbc37b3ec7e1a83fc1dea35832
Muzna Amir <muznazuberi123@gmail.com>
2024-03-30 10:28:19 -0700

    Create a to_do_app using inquirer in typescript

---
+++ To_do_list/index.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+//Let's Create TO_DO_APP
+import inquirer from "inquirer";
+import chalk from "chalk";
+let to_do_list = [];
+let to_do_loops = true;
+console.log(chalk.yellowBright(" Hi! Wellcome to the to_do_app! I hope you like this app "));
+while (to_do_loops) {
+    let To_do_Apps = await inquirer.prompt([
+        {
+            name: "grocery_items",
+            type: "input",
+            message: " What do you want to add grocery_items in your to_do_list? ",
+        },
+        {
+            name: "add_more_items",
+            type: "confirm",
+            message: " Do you want to add_more_items? ",
+        },
+    ]);
+    to_do_list.push(To_do_Apps.grocery_items);
+    to_do_loops = To_do_Apps.add_more_items;
+    console.log(to_do_list);
+}
+;
